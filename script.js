ac312cf9ce710dc39caecfe2df359095429c2bb1
PranayAWani <pranaywani04@gmail.com>
2024-11-20 17:56:33 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Select the container element
+const container = document.querySelector('.container');
+
+// Scroll left function
+function scrollLeft() {
+    container.scrollBy({
+        right: 300, // Adjust scroll distance as needed
+        behavior: 'smooth',
+    });
+}
+
+// Scroll right function
+function scrollRight() {
+    container.scrollBy({
+        left: 300, // Adjust scroll distance as needed
+        behavior: 'smooth',
+    });
+}
 
 
 document.getElementById('login-button').addEventListener('click', () => {
